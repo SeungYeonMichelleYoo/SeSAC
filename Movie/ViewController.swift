2a9a3ad2577ebcd39b469c487d26797691ff19cd
michelle <lune111200@gmail.com>
2022-07-05 16:28:26 +0900

    Netflix-added

--- Movie/ViewController.swift
+++ Movie/ViewController.swift
@@ -9,10 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var MovieBtn: UIButton!
+    @IBOutlet weak var MovieBtn2: UIButton!
+    @IBOutlet weak var MovieBtn3: UIButton!
+    
+    @IBOutlet weak var backgroundUIImage: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        MovieBtn.layer.cornerRadius = 20
+        MovieBtn.layer.cornerRadius = MovieBtn.frame.height/2
+        MovieBtn.layer.borderWidth = 5
+        MovieBtn.layer.borderColor = UIColor.blue.cgColor
+        
+        MovieBtn2.layer.cornerRadius = 100
+        MovieBtn2.layer.borderWidth = 3
+        MovieBtn2.layer.borderColor = UIColor.red.cgColor
+        
+        MovieBtn3.layer.cornerRadius = 120
+        MovieBtn3.layer.borderWidth = 2
+        MovieBtn3.layer.borderColor = UIColor.yellow.cgColor
+    }
+    
+    @IBAction func playBtnClicked(_ sender: UIButton) {
+        backgroundUIImage.image = UIImage(named: "movie\(Int.random(in:1...4))")
     }
 }
 
