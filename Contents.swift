d7914c26aef84cf0406bed31f1065bd3b1267c9a
michelle <lune111200@gmail.com>
2022-07-12 19:07:04 +0900

    RawStrings

---
+++ Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+//var notice = "온라인 라이브 수업은 Zoom 으로 진행됩니다."
+//print(notice)
+
+//var notice = "온라인 라이브 수업은 \"Zoom\" 으로 진행됩니다."
+//print(notice)
+
+//var notice = #"온라인 라이브 수업은 Zoom 으로 진행됩니다."#
+//print(notice)
+
+//var rawNotice = #"\\\온라인 라이브 수업\\\은 \"Zoom\" 으로 진행됩니다."#
+//print(rawNotice)
+//
+//
+//var rawNotice = #"온라인 \\\ 라이브 수업 \#n\#n\#n은 Zoom으로 진행됩니다."#
+//print(rawNotice)
+
+var rawNotice = ###"온라인 \\\ 라이브 수업 \###n\###n\###n은 Zoom으로 진행됩니다."###
+print(rawNotice)
