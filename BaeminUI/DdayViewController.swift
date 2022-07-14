1d1142bc4aa23f4f82780e7ce25af48dc50676b5
michelle <lune111200@gmail.com>
2022-07-14 22:20:25 +0900

    Dday update

---
+++ BaeminUI/DdayViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DdayViewController.swift
+//  BaeminUI
+//
+//  Created by SeungYeon Yoo on 2022/07/14.
+//
+
+import UIKit
+
+class DdayViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+  
+    @IBOutlet var imageViewCollection: [UIImageView]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        cornerChange()
+    }
+    
+    private func configureUI() {
+           setAttributes()
+       }
+       
+       private func setAttributes() {
+           if #available(iOS 14.0, *) {
+               // iOS 14 이상
+               datePicker.preferredDatePickerStyle = .inline
+           } else {
+               // iOS 14 미만
+               datePicker.preferredDatePickerStyle = .wheels
+           }
+           datePicker.datePickerMode = .dateAndTime
+           datePicker.locale = Locale(identifier: "ko-KR")
+           datePicker.timeZone = .autoupdatingCurrent
+       }
+    
+    @IBAction func DatePickerChanged(_ sender: UIDatePicker) {
+        
+    }
+    
+    func cornerChange() {
+        for i in imageViewCollection {
+            i.layer.cornerRadius = 8
+        }
+    }
+    
+
+}
