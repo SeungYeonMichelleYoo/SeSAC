cec7786a4ceadcc159f068d86be2d3403cc21bc2
michelle <lune111200@gmail.com>
2022-08-03 21:59:49 +0900

    collection view added

---
+++ SeSAC2LectureNetworkBasic/ImageSearchCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageSearchCollectionViewCell.swift
+//  SeSAC2LectureNetworkBasic
+//
+//  Created by SeungYeon Yoo on 2022/08/03.
+//
+
+import UIKit
+
+class ImageSearchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ SeSAC2LectureNetworkBasic/ImageSearchViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ImageSearchViewController.swift
+//  SeSAC2LectureNetworkBasic
+//
+//  Created by SeungYeon Yoo on 2022/08/03.
+//
+
+import UIKit
+
+import Alamofire
+import SwiftyJSON
+
+class ImageSearchViewController: UIViewController,                    UICollectionViewDelegate, UICollectionViewDataSource
+{
+   
+    @IBOutlet weak var mainCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mainCollectionView.delegate = self
+        mainCollectionView.dataSource = self
+
+        fetchImage()
+    }
+    
+    var fetchedList: [String] = []
+    
+    //fetchImage, requestImage, callRequestImage, getImage... -> response에 따라 네이밍을 설정해주기도 함.
+    func fetchImage() {
+        let text = "과자".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        let url = EndPoint.imageSearchURL + "query=\(text)&display=30&start=1"
+        
+        let header: HTTPHeaders = ["X-Naver-Client-Id": APIKey.NAVER_ID, "X-Naver-Client-Secret": APIKey.NAVER_SECRET]
+        
+        AF.request(url, method: .get, headers: header).validate(statusCode: 200...500).responseJSON { [self] response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                //print("JSON: \(json)")
+                             
+                let list = json["items"].arrayValue
+                for object in list {
+                    let link = object["link"].stringValue
+                    self.fetchedList.append(link)
+                }
+                print(self.fetchedList.count)
+                DispatchQueue.main.async {
+                    self.mainCollectionView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fetchedList.count
+    }
+        
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageSearchCollectionViewCell", for: indexPath) as? ImageSearchCollectionViewCell else {
+            return UICollectionViewCell()
+        
+        }
+        let imageUrl = URL(string: fetchedList[indexPath.item])
+        cell.imageView.image = UIImage(named: fetchedList[indexPath.item])
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: imageUrl!)
+            DispatchQueue.main.async {
+                cell.imageView.image = UIImage(data: data!)
+        }
+      }
+        return cell
+    }
+}
