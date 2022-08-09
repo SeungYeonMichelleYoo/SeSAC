45a8716adb2facbd02ba322298558ee4936dcd7c
michelle <lune111200@gmail.com>
2022-08-10 07:21:44 +0900

    update

---
+++ UpgradeTMDB/Netflix/CardView.swift
@@ -0,0 +1,15 @@
+//
+//  CardView.swift
+//  UpgradeTMDB
+//
+//  Created by SeungYeon Yoo on 2022/08/09.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var contentLabel: UILabel!
+}
---
+++ UpgradeTMDB/Netflix/NetflixViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NetflixViewController.swift
+//  UpgradeTMDB
+//
+//  Created by SeungYeon Yoo on 2022/08/09.
+//
+
+import UIKit
+
+class NetflixViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
