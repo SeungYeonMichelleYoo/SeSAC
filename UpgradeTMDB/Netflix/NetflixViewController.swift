abf854c49b96977dac3e93344d1a89ee2eebb2b2
michelle <lune111200@gmail.com>
2022-08-11 06:15:29 +0900

    tableVC update

--- UpgradeTMDB/Netflix/NetflixViewController.swift
+++ UpgradeTMDB/Netflix/NetflixViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class NetflixViewController: UIViewController {
     
-    @IBOutlet weak var contentCollectionView: UICollectionView!
     @IBOutlet weak var mainTableView: UITableView!
     
     let color: [UIColor] = [.red, .systemPink, .lightGray, .yellow, .black]
@@ -29,13 +28,11 @@ class NetflixViewController: UIViewController {
 
         mainTableView.delegate = self
         mainTableView.dataSource = self
-        
-        contentCollectionView!.delegate = self
-        contentCollectionView!.dataSource = self
        
     }
 }
 
+//MARK: - TableView
 extension NetflixViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -49,17 +46,31 @@ extension NetflixViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as? MainTableViewCell else { return UITableViewCell() }
         
-        cell.backgroundColor = .yellow
+        cell.backgroundColor = .black
+        cell.titleLabel.text = "넷플릭스 장르별 컨텐츠"
+        cell.titleLabel.textColor = .white
         cell.contentCollectionView.backgroundColor = .lightGray
+        
         cell.contentCollectionView.delegate = self
         cell.contentCollectionView.dataSource = self
+        
+        cell.contentCollectionView.collectionViewLayout = collectionViewLayout()
+        cell.contentCollectionView.isPagingEnabled = true //자석처럼 셀이 넘어감(오픈소스:ImageSlideShow). device width와 셀이 같을 때
+        
         cell.contentCollectionView.tag = indexPath.section // 각 셀 구분 짓기
         cell.contentCollectionView.register(UINib(nibName: "CardCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CardCollectionViewCell")
+       
+        cell.contentCollectionView.reloadData() //인덱스 오류 해결
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
 }
 
+//MARK: -CollectionView
 extension NetflixViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return numberList[collectionView.tag].count
@@ -69,11 +80,21 @@ extension NetflixViewController: UICollectionViewDelegate, UICollectionViewDataS
         
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCollectionViewCell", for: indexPath) as? CardCollectionViewCell else { return UICollectionViewCell() }
         
-            cell.cardView.posterImageView.backgroundColor = .black
+            cell.cardView.posterImageView.backgroundColor = .systemIndigo
             cell.cardView.contentLabel.textColor = .white
             cell.cardView.contentLabel.text = "\(numberList[collectionView.tag][indexPath.item])"
                     
         return cell
+    }
+    
+    func collectionViewLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 3)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        return layout
     }
 }
 
